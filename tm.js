db7175fc8ceb89c3d3a83b17339d874311168031
ldxysbl <109933802+ldxysbl@users.noreply.github.com>
2022-09-12 13:14:29 +0800

    Create tm.js

---
+++ tm.js
@@ -0,0 +1,6 @@
+
+		function sj(){
+			var date=new Date();
+			var strDate=date.getHours()+':'+date.getMinutes()+':'+date.getSecond()+':'+date.getMIlliseconds();
+			document.getElementById("time").innerHTML=sreDate;
+		}
